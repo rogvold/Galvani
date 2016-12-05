0b7ad10d968165f6237f4207f7503c73ba796190
rogvold <sha-sabir@yandex.ru>
2016-12-06 01:53:41 +0300

    initial commit

---
+++ GalvaniWebApp/src/constants/AccountsConstants.js
@@ -0,0 +1,14 @@
+/**
+ * Created by sabir on 29.11.16.
+ */
+
+export const PARSE_APP_ID = 'qBQxbpKxB5zvFSClPfGpz2M9lBxGLZgAeKTi6cKf';
+export const PARSE_JS_KEY = '2800npjTtuJd7r2G3q4ooC2hYTXo0c4FAUKgQf2w';
+export const PARSE_REST_API_KEY = 'cHjRVnZDqLi2Hfu4SSRebWPWWZNg68E2BPHZnl0f';
+
+export const  PUSHER_APP_ID = '277327';
+export const  PUSHER_KEY = '73e66eb390a74eb8b083';
+export const  PUSHER_SECRET = '966542dce2dc8ecc8446';
+
+export const LOGO_URL = 'http://media.lpgenerator.ru/images/339161/logo.png#size_298x60';
+export const COMPANY_NAME = undefined;
